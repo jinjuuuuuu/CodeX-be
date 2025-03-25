745aeebd404e44f5ff753e05161d03eefb461455
BuildTools <unconfigured@null.spigotmc.org>
2025-03-25 23:19:57 +0900

    initialize setting

--- code-judge/src/main/java/com/example/code_judge/repository/SubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.code_judge.repository;
-
-import com.example.code_judge.domain.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
