1a06dbf0a7d81fd68ab428ba04b7e73342b64141
한진주 <hjinju828@gmail.com>
2025-04-16 15:00:24 +0900

    Feat - @EnableCaching 추가

--- code-judge/src/main/java/com/example/code_judge/CodeJudgeApplication.java
+++ code-judge/src/main/java/com/example/code_judge/CodeJudgeApplication.java
@@ -2,8 +2,10 @@ package com.example.code_judge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CodeJudgeApplication {
     public static void main(String[] args) {
         SpringApplication.run(CodeJudgeApplication.class, args);
